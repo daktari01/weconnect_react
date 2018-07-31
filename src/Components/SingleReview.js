1bab9f01b98c11c2ed6b1c6b584bda136bc548f0
James Dindi <dindijjames@gmail.com>
2018-07-31 11:19:16 +0300

    [feature #157277165] Get Reviews User can retrieve reviews from the db

---
+++ src/Components/SingleReview.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import company_logo from "../static/img/logos/weConnectCircle.png";
+import user_avatar from "../static/img/users/user_avatar.png";
+
+const SingleReview = ({ review }) => (
+  <div id="review_item" className="row">
+    <div className="col-sm-3 pull-left" id="user_holder">
+      <img
+        src={user_avatar}
+        id="user_avatar"
+        className="mx-auto img-fluid img-circle d-block"
+        alt="avatar"
+      />
+    </div>
+    <div className="col-sm-9 pull-right">
+      <h4>{review.review_title}</h4>
+      <div className="star-rating pull-left">
+        <span className="fa fa-star-o" data-rating="1" />
+        <span className="fa fa-star-o" data-rating="2" />
+        <span className="fa fa-star-o" data-rating="3" />
+        <span className="fa fa-star-o" data-rating="4" />
+        <span className="fa fa-star-o" data-rating="5" />
+        <input
+          type="hidden"
+          name="whatever1"
+          className="rating-value"
+          value="2.56"
+        />
+      </div>
+      <br />
+      <br />
+      <p>{review.review_text}</p>
+      <hr />
+    </div>
+  </div>
+);
+
+SingleReview.propTypes = {
+  review: PropTypes.object
+};
+export default SingleReview;
