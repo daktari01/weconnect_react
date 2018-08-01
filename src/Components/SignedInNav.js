e0aa83a0c56b176c5bb9c93bc050f7d5d0e18cc5
James Dindi <dindijjames@gmail.com>
2018-08-01 12:05:30 +0300

    [feature #157277417] User can logout

--- src/Components/SignedInNav.js
+++ src/Components/SignedInNav.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 import logo from "../static/img/logos/weConnect.png";
 
 class SignedInNav extends Component {
+  state = {
+    fireRedirect: false
+  }
+  handleLogout = event => {
+    const access_token = localStorage.getItem("access_token");
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/api/v2/auth/logout", {}, {
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+          "x-access-token": access_token
+        }
+      })
+      .then(response => {
+        console.log(response.data);
+        this.setState({ fireRedirect: true });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
   render() {
+      const { fireRedirect } = this.state;
     return (
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-faded">
@@ -30,13 +55,14 @@ class SignedInNav extends Component {
             </ul>
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link" href="login.html">
+                <a className="nav-link" href="#" onClick={this.handleLogout}>
                   Logout
                 </a>
               </li>
             </ul>
           </div>
         </nav>
+        {fireRedirect && <Redirect to={"/login"} />}
       </div>
     );
   }
